708ca159134311de9f233bad3d924631667c9126
Macelo <maceloaugusto066@gmail.com>
2025-02-28 13:40:01 -0300

    site tabelas transformado.

---
+++ projetos/emprea-table/assets/js/scripts.js
@@ -0,0 +1,11 @@
+window.addEventListener('scroll', () => {
+    const navBar = document.getElementById("nav-bar");
+
+    if (window.scrollY === 0){
+        navBar.classList.remove("bord")
+        navBar.classList.add("bordLateral")
+    }else{
+        navBar.classList.remove("bordLateral")
+        navBar.classList.add("bord");
+    }
+})
