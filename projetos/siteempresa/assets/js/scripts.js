41c2a2bfef6f656bd0b94b418a556532ed63348d
Macelo <maceloaugusto066@gmail.com>
2025-03-11 21:37:40 -0300

    Alguns ajustes.

--- projetos/siteempresa/assets/js/scripts.js
+++ projetos/siteempresa/assets/js/scripts.js
@@ -1,11 +1,11 @@
-window.addEventListener('scroll', () => {
-    const navBar = document.getElementById("nav-bar");
+// window.addEventListener('scroll', () => {
+//     const navBar = document.getElementById("nav-bar");
 
-    if (window.scrollY === 0){
-        navBar.classList.remove("bord")
-        navBar.classList.add("bordLateral")
-    }else{
-        navBar.classList.remove("bordLateral")
-        navBar.classList.add("bord");
-    }
-})
+//     if (window.scrollY === 0){
+//         navBar.classList.remove("bord")
+//         navBar.classList.add("bordLateral")
+//     }else{
+//         navBar.classList.remove("bordLateral")
+//         navBar.classList.add("bord");
+//     }
+// })
