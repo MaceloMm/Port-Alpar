647cdf2c2a199c88d6156a3e47a76ad2c22aebb9
Macelo <maceloaugusto066@gmail.com>
2025-05-02 10:03:33 -0300

    aula 02/05

---
+++ projetos/previsao/assets/scripts/main.js
@@ -0,0 +1,43 @@
+const app = angular.module('weatherAPP', []);
+const apiKey = "";
+const lang = "pt_br";
+const units = "metric";
+
+// sadsa 52c34bb1fec8a47fe2676ce765d114b5
+
+app.controller('WeatherController', function ($scope, $http) {
+    $scope.cityName = '';
+    $scope.city = '';
+    $scope.cardActive = false;
+    $scope.temperature = "";
+    $scope.feelsLike = "";
+    $scope.minTemperature = "";
+    $scope.maxTemperature = "";
+    $scope.humidity = "";
+    $scope.windVelocity = "";
+    $scope.windOrietation = "";
+    $scope.iconURL = "";
+
+    $scope.callAPI = async () => {
+        const city = $scope.city || localStorage.getItem('city') || 'São Paulo';
+
+        const reponse = await $http.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}&lang=${lang}`)
+        const data = reponse.data;
+        const icon = data.weather[0].icon;
+
+        $scope.cityName = data.name;
+        $scope.iconURL = `https://openweathermap.org/img/wn/${icon}@4x.png`;
+        $scope.temperature = Math.round(data.main.temp);
+        $scope.feelsLike  = Math.round(data.main.feels_like);
+        $scope.minTemperature = Math.round(data.main.temp_min);
+        $scope.maxTemperature = Math.round(data.main.temp_max);
+        $scope.humidity = data.main.humidity.toLocaleString();
+        $scope.windVelocity = data.wind.speed.toLocaleString();
+        $scope.windOrietation = data.wind.deg.toLocaleString();
+        $scope.cardActive = true;
+        localStorage.setItem('city', data.name);
+        $scope.$apply();
+    }
+
+    $scope.callAPI()
+})
---
+++ projetos/previsao/assets/scripts/script.js
@@ -0,0 +1,46 @@
+const apiKey = "52c34bb1fec8a47fe2676ce765d114b5";
+const lang = "pt_br";
+const units = "metric";
+
+const cardEl = document.querySelector(".card");
+const iconEl = document.querySelector(".icon");
+const cidadeEl = document.querySelector(".cidade");
+const tempEL = document.querySelector(".temp");
+const tempsenseEL = document.querySelector(".temp-sense span");
+const minEL = document.querySelector(".min");
+const maxEL = document.querySelector(".max");
+const humidityEL = document.querySelector(".humidity");
+const fotoEL = document.querySelector(".foto img");
+const fotopEl = document.querySelector(".foto span");
+const inputEL = document.querySelector(".input input");
+const buttonEl = document.querySelector(".input button");
+
+function callApi() {
+
+    const city = inputEL.value;
+
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}&lang=${lang}`
+    )
+        .then((response) => response.json())
+        .then((response) => {
+            const data = response.json()
+            const icon = response.weather[0].icon;
+            const iconUrl = `https://openweathermap.org/img/wn/${icon}@4x.png`
+            iconEl.src = iconUrl;
+
+            cidadeEl.innerHTML = city;
+            tempEL.innerHTML = response.main.temp;
+            tempsenseEL.innerHTML = response.main.feels_like;
+            minEL.innerHTML = response.main.temp_min;
+            maxEL.innerHTML = response.main.temp_max;
+            humidityEL.innerHTML = response.main.humidity;
+            fotopEl.innerHTML = response.wind.speed;
+            fotoEL.style.transform = `rotate(${response.wind.deg}deg)`
+            cardEl.classList.add("active");
+
+        });
+}
+
+
+buttonEl.addEventListener('click', callApi)
